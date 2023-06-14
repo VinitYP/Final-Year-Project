7ff7c8baf6f0b18b50fc0d740b5df33fc29a7f23
Vinit Pardhi <91457234+VinitYP@users.noreply.github.com>
2023-06-14 12:06:16 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/LjQH9QJr0LEsqMPecz9ze85mp-c5E-IS',
+      url: '' // DELETED,
       accounts: ['']  // eslint-disable-line disable or delete before share
     }
   }
